469096aafc03e3b67fb8a9b40598968371471bda
daniela.blumberg <daniela.blumberg@uni-konstanz.de>
2023-07-12 14:43:23 +0200

    draft new version

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ var legend = d3.select('#legend').append('svg')
     .append("g")
     .attr("transform", "translate(0," + margins.top + ")");
 
+var svg2 = d3.select("#plot2").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+
+
+var legend2 = d3.select('#legend2').append('svg')
+    .attr('width', 300)
+    .attr('height', height)
+    .append("g")
+    .attr("transform", "translate(0," + margins.top + ")");
+
 //define scales
 var xScale = d3.scaleLinear().range([50, width-100]);
 var yScale = d3.scaleLinear().range([height-100, 50]);
@@ -175,5 +188,118 @@ d3.dsv(";","data/topics.csv", function(d){
          */
 });
 
+//second viz with raw data as input to BERTopic
+d3.dsv(";","data/topics_raw_data.csv", function(d){
+    return {topic_name : d.Name, topic_id : d.Topic_id, Count : +d.Count, Count_men : +d.Count_men, Count_women : +d.Count_women, x : +d.x, y : +d.y, Representation : d.Representation, Representative_Docs: d.Representative_Docs}
+}).then(function(d){
+    var data = d;
+    data.sort((a,b) => b.Count - a.Count); //sort according to overall occurence
+    //var data20 = data.slice(0,20); //take only top 20 topics
+    console.log(data);
 
+    // adjust domains of defined scales to min and max of data
+    xScale.domain(d3.extent(data, d => d.x));
+    yScale.domain(d3.extent(data, d => d.y));
+    sizeScale.domain(d3.extent(data, d => d.Count));
+
+    // calculate relative ocuurences of topics in AskMen/Women -> adjusted by total posts in AskMen/Women
+    for (let i = 0; i < data.length; i++) {
+        data[i].share_men = data[i].Count_men/num_posts_men ; 
+        data[i].share_women = data[i].Count_women/num_posts_women;
+        data[i].topic_share = data[i].share_men / (data[i].share_men + data[i].share_women)
+    };
+    
+
+    // add topics
+    svg2.selectAll('.topic').data(data).enter().append('circle')
+        .attr('class', 'topic')
+        .attr('cx', d=> xScale(d.x))
+        .attr('cy', d => yScale(d.y))
+        .style('r', d => sizeScale(d.Count)/2)
+        .attr('fill', d => colorScale(d.topic_share))
+        .on("mouseover", function() {
+            d3.select(this).style("cursor", "pointer");
+        })
+        // add tooltip
+        .on("mouseenter",function(event, d){
+            d3.select("#tooltip").style("left",(event.pageX+30)+"px").style("top",(event.pageY-20)+"px").style("display","block")
+            .html(
+                '<strong>' + d.topic_name + ' (id ' + d.topic_id + '): </strong> <br><br> - ' +
+                d.Count + ' posts (' + Math.round(d.topic_share * 10000) / 100 + '% AskMen, ' +
+                Math.round((1 - d.topic_share) * 10000) / 100 + '% AskWomen)' 
+                + '<br> - Top 10 words: ' + d.Representation
+                //+ '<br> - representaive post: ' + d.Representative_Docs.slice(0, 1000) + '...'
+              );
+        })
+        .on("mousemove",function(event, d){
+            d3.select("#tooltip").style("left",(event.pageX+30)+"px").style("top",(event.pageY-20)+"px");
+        })
+        .on("mouseout",function(){
+            d3.select("#tooltip").style("display","none");
+            d3.select(this).style("cursor", "default");
+        });
+
+    // add color legend
+    var gradient = legend2.append("defs")
+        .append("linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "0%");
+
+    gradient.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#ca0020"); 
+
+    gradient.append("stop")
+        .attr("offset", "50%")
+        .attr("stop-color", "gray");
+
+    gradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#0571b0");
+
+    legend2.append('text')
+        .attr('x',0)
+        .attr('y',80)
+        .style('text-anchor', 'start')
+        .html('Topic is present in:');
+
+    legend2.append("rect")
+        .attr("width", 300)
+        .attr("height", 20)
+        .attr('x',0)
+        .attr('y',100)
+        .style("fill", "url(#gradient)");
+
+    legend2.append('text')
+        .attr('x',0)
+        .attr('y',140)
+        .style('text-anchor', 'start')
+        .html('AskWomen');
+    legend2.append('text')
+        .attr('x',150)
+        .attr('y',140)
+        .style('text-anchor', 'center')
+        .html('both');
+    legend2.append('text')
+        .attr('x',300)
+        .attr('y',140)
+        .style('text-anchor', 'end')
+        .html('AskMen');
+
+    legend2.append('text')
+        .attr('x',10)
+        .attr('y',250)
+        .style('text-anchor', 'start')
+        .html('- size: overall occurence of topic');
+    
+    legend2.append('text')
+        .attr('x',10)
+        .attr('y',300)
+        .style('text-anchor', 'start')
+        .html('- position: 2-dim topic embedding');
+
+});
 
